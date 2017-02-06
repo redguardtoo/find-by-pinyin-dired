2c48434637bd63840fca4d2c6cf9ebd5dd44658f
Chen Bin <chenbin.sh@gmail.com>
2017-02-06 20:49:26 +1100

    use pinyinlib and can search in project root

--- find-by-pinyin-dired-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "find-by-pinyin-dired" "0.0.2"
-                "Find file by first Pinyin characters of Chinese Hanzi")
--- pkg.sh
+++ pkg.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-pkg=find-by-pinyin-dired-0.0.2
+name=find-by-pinyin-dired
+version=0.0.3
+pkg=$name-$version
 mkdir $pkg
-cp README.org $pkg
 cp *.el $pkg
+cat << EOF > $pkg/$name-pkg.el
+(define-package "$name" "$version"
+                "whatever")
+EOF
 if [[ `uname -s` == *Darwin* ]]; then
    COPYFILE_DISABLE="" tar cvf $pkg.tar $pkg/
 else
